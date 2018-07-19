8068fb151b6f59d07a5d2bdc34f34d52ce330a26
sageliu <1451953028@qq.com>
2018-07-19 21:34:51 +0800

    update

---
+++ base/base_driver.py
@@ -0,0 +1,26 @@
+#coding = utf-8
+from appium import webdriver
+class Base_Driver:
+    '''
+    获取设备信息
+    '''
+    def get_driver(self,driverName):
+        '''
+        获取driver驱动
+        :param driverName:
+        :return:driver
+        '''
+
+        capabilities = {
+            'platformName' : 'Android',
+            'deviceName' : deviceName,
+            'appPackage' : '',
+            'appActivity' : '',
+            'app' : '',
+            'automationName' : 'appium',
+        }
+        driver = webdriver.Remote('')
+        return driver
+
+
+
